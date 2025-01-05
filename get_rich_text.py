6293d6dcb75f8f64454ca3c0f8d129016bd59597
root <root@v2526088.hosted-by-vdsina.ru>
2025-01-05 14:10:55 +0300

    sync commit

--- get_rich_text.py
+++ get_rich_text.py
@@ -26,7 +26,7 @@ service = Service(ChromeDriverManager().install())
 
 def get_rich_text():
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)  # Увеличенное время ожидания
     rows = fetch_unparsed_links()
 
     updated_count = 0
@@ -34,7 +34,7 @@ def get_rich_text():
     for row in rows:
         link, title = row
         driver.get(link)
-        time.sleep(5)
+        time.sleep(10)  # Увеличенное время ожидания загрузки страницы
 
         try:
             if 'подкаст' in title.lower():
@@ -42,32 +42,39 @@ def get_rich_text():
                 logger.info(f"Новость по ссылке '{link}' содержит подкаст и обновлена.")
                 continue
 
-            # Ожидание загрузки элемента
-            rich_text_element = wait.until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'Inner-Page__content'))
+            # Ожидание загрузки всех элементов с нужным классом и их видимости
+            rich_text_elements = wait.until(
+                EC.visibility_of_all_elements_located((By.CLASS_NAME, 'Inner-Page__content'))
             )
-            rich_text = rich_text_element.text
 
-            # Удаление текста из элементов с классом 'alt_under_img'
-            alt_text_elements = driver.find_elements(By.CLASS_NAME, 'alt_under_img')
-            for element in alt_text_elements:
-                alt_text = element.text
-                rich_text = rich_text.replace(alt_text, '')
+            if rich_text_elements:
+                # Предположим, что нужно работать с первым элементом, если их несколько
+                rich_text_element = rich_text_elements[0]
+                rich_text = rich_text_element.text
 
-            # Добавление пустой строки после каждого абзаца
-            paragraphs = rich_text.split('\n')
-            rich_text_with_newlines = '\n\n'.join(paragraphs)
+                # Удаление текста из элементов с классом 'alt_under_img'
+                alt_text_elements = driver.find_elements(By.CLASS_NAME, 'alt_under_img')
+                for element in alt_text_elements:
+                    alt_text = element.text
+                    rich_text = rich_text.replace(alt_text, '')
 
-            update_parsed(link, rich_text_with_newlines)
-            updated_count += 1
-            logger.info(f"Новость по ссылке '{link}' обновлена с rich_text")
+                # Добавление пустой строки после каждого абзаца
+                paragraphs = rich_text.split('\n')
+                rich_text_with_newlines = '\n\n'.join(paragraphs)
+
+                update_parsed(link, rich_text_with_newlines)
+                updated_count += 1
+                logger.info(f"Новость по ссылке '{link}' обновлена с rich_text")
+            else:
+                logger.warning(f"Rich text элементы не найдены по ссылке '{link}'")
+                update_parsed(link, rich_text=None)
 
         except NoSuchElementException:
             logger.warning(f"Rich text отсутствует в новости по ссылке '{link}'")
             update_parsed(link, rich_text=None)
         except Exception as e:
             update_parsed(link, rich_text=None)
-            logger.error(f"Ошибка при извлечении rich_text по ссылке '{link}': no_rich_text")
+            logger.error(f"Ошибка при извлечении rich_text по ссылке '{link}': {str(e)}")
 
     if updated_count == 0:
         logger.info("Обновлять нечего, все новости уже обновлены.")
